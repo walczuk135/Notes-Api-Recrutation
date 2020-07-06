147c1069f2ade75ab67f14f4641fff710675ac4b
Paweł <walczuk135@gmail.com>
2020-07-06 20:36:33 +0200

    Add  NoteServiceTest ,
    Add to NoteServiceTest test testFindByIdSuccess is not passed

---
+++ src/test/java/com/rest/api/restapi/sevice/NoteServiceTest.java
@@ -0,0 +1,39 @@
+package com.rest.api.restapi.sevice;
+
+import com.rest.api.restapi.model.Note;
+import com.rest.api.restapi.service.NotesService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class NoteServiceTest {
+    @Autowired
+    private NotesService service;
+
+    @MockBean
+    private NoteRepository repository;
+
+    @Test
+    void testFindByIdSuccess() {
+        // Setup our mock
+        Note mockNote = new Note(1, "Note number one", "Note one example lorrem ipsum");
+        doReturn(Optional.of(mockNote)).when(repository).findById("reviewId");
+
+        // Execute the service call
+        Optional<Note> returnedReview = service.findById(1L);
+
+        // Assert the response
+        Assertions.assertTrue(returnedReview.isPresent(), "Review was not found");
+        Assertions.assertSame(returnedReview.get(), mockNote, "Review should be the same");
+    }
+}
